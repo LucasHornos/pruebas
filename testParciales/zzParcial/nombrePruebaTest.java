c38c162716d4cc3c18ea26aa89a2c56b7b81b315
“Lucas <lucashornos04@gmail.com>
2023-05-09 18:38:04 -0300

    PrimerCommit

---
+++ testParciales/zzParcial/nombrePruebaTest.java
@@ -0,0 +1,14 @@
+package zzParcial;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class nombrePruebaTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
